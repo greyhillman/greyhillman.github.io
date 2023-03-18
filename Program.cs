f4523a4c2cfdd3285f815e28ccca21bf1ba82c17
Grey Hill <infogreytech@gmail.com>
2023-03-18 12:57:31 -0700

    Create initial C# build system

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Site
+{
+    public static class Build
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
